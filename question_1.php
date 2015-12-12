7d94b6d96fe3ac129f4c1e951875abeb5d0c4877
Toby Nieboer <toby@nieboer.com.au>
2015-12-12 14:23:41 +1100

    Software Engineer Test - Toby Nieboer

--- question_1.php
+++ question_1.php
@@ -1,5 +1,16 @@
 <?php
+/**
+ * This file generates a list of customers and their occupations.
+ *
+ * @package		SoftwareEngineerTest
+ * @subpackage	Question1
+ * @license		http://opensource.org/licenses/gpl-license.php  GNU Public License
+ * @author		Toby Nieboer <toby@nieboer.com.au>
+ */
+ 
 namespace SoftwareEngineerTest;
+use \PDO;
+use \PDOException;
 
 // Question 1a
 
@@ -9,8 +20,32 @@ $DB_USER = 'test';
 $DB_PASS = 'test';
 
 // write your sql to get customer_data here
-$sql = "...";
 
+try {
+    $connect = new PDO('mysql:host='.$DB_HOST.';dbname='.$DB_NAME, $DB_USER, $DB_PASS);
+} catch (PDOException $e) {
+    print "Connection error: " . $e->getMessage() . "<br/>";
+    die();
+}
+
+$sql = "SELECT customer_id, 
+		first_name, 
+		last_name, 
+		CASE  
+			WHEN occupation_name IS NULL THEN 'un-employed' 
+			ELSE occupation_name 
+			END 
+		AS occupation_name
+		FROM customer LEFT JOIN customer_occupation ON customer.customer_occupation_id = customer_occupation.customer_occupation_id";
+if($_GET['occupation_name']){
+	$sql .= " WHERE occupation_name = :occupation_name";
+	$query = $connect->prepare($sql);
+	$query->bindParam(':occupation_name', $_GET['occupation_name'], PDO::PARAM_STR);
+	$query->execute();
+} else {
+	$query = $connect->prepare($sql);
+	$query->execute();
+}
 ?>
 
 <h2>Customer List</h2>
@@ -24,5 +59,16 @@ $sql = "...";
 	</tr>
 
 	<!-- Write your code here -->
-
+	<?php
+	while($row = $query->fetch(PDO::FETCH_ASSOC)){
+	?>	
+		<tr>
+			<td><?php echo $row['customer_id'];?></td>	
+			<td><?php echo $row['first_name'];?></td>	
+			<td><?php echo $row['last_name'];?></td>	
+			<td><?php echo $row['occupation_name'];?></td>
+		</tr>
+	<?php		
+	}
+	?>
 </table>
--- question_2.php
+++ question_2.php
@@ -1,23 +1,170 @@
 <?php
+/**
+ * This file handles bronze, silver and gold customers.
+ *
+ * @package		SoftwareEngineerTest
+ * @subpackage	Question2
+ * @license		http://opensource.org/licenses/gpl-license.php  GNU Public License
+ * @author		Toby Nieboer <toby@nieboer.com.au>
+ */
 namespace SoftwareEngineerTest;
+use \InvalidArgumentException;
 
 // Question 2 & 3 & 4
 
 /**
  * Class Customer
- */
+ * 
+ * @package		SoftwareEngineerTest
+ * @subpackage	Question2
+ * @author		Toby Nieboer <toby@nieboer.com.au>
+*/
 abstract class Customer {
+	/** @var string Customer ID */
 	protected $id;
+	/** @var int Customer balance */
 	protected $balance = 0;
 
+	/**
+	 * Constructor
+	 * 
+	 * @param		int			$id
+	 * @return		void		
+	*/
 	public function __construct($id) {
 		$this->id = $id;
 	}
 
+	/**
+	 * Returns current value of $balance
+	 * 
+	 * @return	int		
+	*/
 	public function get_balance() {
 		return $this->balance;
 	}
+
 }
 
-
 // Write your code below
+
+/**
+ * Class Bronze
+ * 
+ * @package		SoftwareEngineerTest
+ * @subpackage	Question2
+ * @author		Toby Nieboer <toby@nieboer.com.au>
+*/
+class Bronze extends Customer {
+	/**
+	 * Increments $balance by $amount
+	 * 
+	 * @param		int			$amount
+	 * @return		void		
+	*/
+	public function deposit($amount){
+		$new_balance = parent::get_balance() + $amount;
+		$this->balance = $new_balance;
+	}
+	/**
+	 * Generates a username consisting of B and 29 semi-random alphanumeric characters
+	 * 
+	 * @return		string		
+	*/
+	public function generate_username(){
+		$username = 'B'.substr(md5(microtime()),-29);
+		return $username;
+		
+	}
+}
+/**
+ * Class Silver
+ * 
+ * @package		SoftwareEngineerTest
+ * @subpackage	Question2
+ * @author		Toby Nieboer <toby@nieboer.com.au>
+*/
+class Silver extends Customer {
+	/**
+	 * Increments $balance by $amount * 1.05
+	 * 
+	 * @param		int			$amount
+	 * @return		void		
+	*/
+	public function deposit($amount){
+		$new_balance = parent::get_balance() + ($amount * 1.05);
+		$this->balance = $new_balance;
+	}
+	/**
+	 * Generates a username consisting of S and 29 semi-random alphanumeric characters
+	 * 
+	 * @return		string		
+	*/
+	public function generate_username(){
+		$username = 'S'.substr(md5(microtime()),-29);
+		return $username;
+		
+	}
+}
+/**
+ * Class Gold
+ * 
+ * @package		SoftwareEngineerTest
+ * @subpackage	Question2
+ * @author		Toby Nieboer <toby@nieboer.com.au>
+*/
+
+class Gold extends Customer {
+	/**
+	 * Increments $balance by $amount * 1.1
+	 * 
+	 * @param		int			$amount
+	 * @return		void		
+	*/
+	public function deposit($amount){
+		$new_balance = parent::get_balance() + ($amount * 1.1);
+		$this->balance = $new_balance;
+	}
+	
+	/**
+	 * Generates a username consisting of G and 29 semi-random alphanumeric characters
+	 * 
+	 * @return		string		
+	*/
+	public function generate_username(){
+		$username = 'G'.substr(md5(microtime()),-29);
+		return $username;
+		
+	}
+}
+
+/**
+ * Class CustFactory
+ * 
+ * @package		SoftwareEngineerTest
+ * @subpackage	Question3
+ * @author		Toby Nieboer <toby@nieboer.com.au>
+*/
+
+class CustFactory {
+
+	/** @var string Customer type */
+	protected $type;
+
+	/**
+	 * Gets customer type based on ID
+	 * 
+	 * @param		int			$custid
+	 * @return		Gold|Silver|Bronze		
+	*/
+	public function get_instance($custid){
+		if(strtolower(substr($custid,0,1)) == 'b'){
+			return $this->type = new Bronze($custid);
+		} elseif(strtolower(substr($custid,0,1)) == 's'){
+			return $this->type = new Silver($custid);
+		} elseif(strtolower(substr($custid,0,1)) == 'g'){
+			return $this->type = new Gold($custid);
+		}
+	throw new InvalidArgumentException($custid.' is not a valid customer ID');
+	}
+}
